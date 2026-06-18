a60870019d4c9918c130fa987d11e8a25e01c31a
Ao <yunqi_ao>
2026-06-18 09:26:45 +0800

    feat:游戏预制体

---
+++ Assets/Scripts/MPGameView.Create.cs
@@ -0,0 +1,43 @@
+using HQ.UIManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 创建游戏中的对象
+/// </summary>
+public partial class MPGameView : AWindow
+{
+
+    /// <summary>
+    /// 创建Block网格
+    /// </summary>
+    private void CreateGrid()
+    {
+
+    }
+
+    /// <summary>
+    /// 创建横着的数字提示
+    /// </summary>
+    private void CreateHorizontalNumber()
+    {
+
+    }
+
+    /// <summary>
+    /// 创建竖着的数字提示
+    /// </summary>
+    private void CreateVerticalNumver()
+    {
+
+    }
+
+    /// <summary>
+    /// 创建分隔线段
+    /// </summary>
+    private void CreateLine()
+    {
+
+    }
+}
